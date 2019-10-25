15d9e01c1c079d0835bad8dd88a8d59db765d412
Loghan Ramassamy <loghan.r@gmail.com>
2019-10-25 17:34:47 +0200

    add method nextPlayer in Players

--- src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -3,7 +3,6 @@ package com.adaptionsoft.games.uglytrivia;
 public class Game {
     Players players;
     final Deck deck;
-    int currentPlayer = 0;
     boolean isGettingOutOfPenaltyBox;
 
     public Game() {
@@ -16,30 +15,27 @@ public class Game {
         players.add(playerName);
     }
 
-    private Player getCurrentPlayer() {
-        return players.getPlayerByIndex(currentPlayer);
-    }
-
     public void roll(int roll) {
-        getCurrentPlayer().print();
+        players.getCurrentPlayer().print();
+
         System.out.println("They have rolled a " + roll); // faute orthographe --> pour plus tard
 
-        boolean isCurrentPlayerInPenaltyBox = getCurrentPlayer().isInPenalty();
+        boolean isCurrentPlayerInPenaltyBox = players.getCurrentPlayer().isInPenalty();
 
         if (isCurrentPlayerInPenaltyBox && isEven(roll)) {
             // Extract in method enterPenaltyBox
             isGettingOutOfPenaltyBox = false;
-            System.out.println(getCurrentPlayer() + " is not getting out of the penalty box");
+            System.out.println(players.getCurrentPlayer() + " is not getting out of the penalty box");
             return;
         }
 
         if (isCurrentPlayerInPenaltyBox && isOdd(roll)) {
             // Extract in method exitPenaltyBox
             isGettingOutOfPenaltyBox = true;
-            System.out.println(getCurrentPlayer() + " is getting out of the penalty box");
+            System.out.println(players.getCurrentPlayer() + " is getting out of the penalty box");
         }
 
-        getCurrentPlayer().move(roll);
+        players.getCurrentPlayer().move(roll);
         currentCategory().print();
         deck.printQuestionCategory(currentCategory());
     }
@@ -53,39 +49,36 @@ public class Game {
     }
 
     private QuestionCategory currentCategory() {
-        return QuestionCategory.getCategoryBy(getCurrentPlayer().getPlace());
+        return QuestionCategory.getCategoryBy(players.getCurrentPlayer().getPlace());
     }
 
     public boolean wasCorrectlyAnswered() { // plusieurs responsabilites? renommer?
-        if (getCurrentPlayer().isInPenalty() && !isGettingOutOfPenaltyBox) {
+        if (players.getCurrentPlayer().isInPenalty() && !isGettingOutOfPenaltyBox) {
             return true;
         }
 
-        if (getCurrentPlayer().isInPenalty() && isGettingOutOfPenaltyBox) {
+        if (players.getCurrentPlayer().isInPenalty() && isGettingOutOfPenaltyBox) {
             System.out.println("Answer was correct!!!!");
         } else {
             System.out.println("Answer was corrent!!!!"); // faute orthographe --> pour plus tard
         }
 
-        getCurrentPlayer().increasePurse();
+        players.getCurrentPlayer().increasePurse();
         return didPlayerWin();
     }
 
     private boolean didPlayerWin() {
-        return getCurrentPlayer().getPurse() != 6;
+        return players.getCurrentPlayer().getPurse() != 6;
     }
 
     public void nextPlayer() {
-        currentPlayer++;
-        if (currentPlayer == players.size()) {
-            currentPlayer = 0;
-        }
+        players.nextPlayer();
     }
 
     public boolean wrongAnswer() { // return always true
         System.out.println("Question was incorrectly answered");
-        System.out.println(getCurrentPlayer() + " was sent to the penalty box");
-        getCurrentPlayer().setInPenalty(true);
+        System.out.println(players.getCurrentPlayer() + " was sent to the penalty box");
+        players.getCurrentPlayer().setInPenalty(true);
         nextPlayer();
         return true;
     }
--- src/main/java/com/adaptionsoft/games/uglytrivia/Players.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Players.java
@@ -3,21 +3,23 @@ package com.adaptionsoft.games.uglytrivia;
 import java.util.ArrayList;
 
 public class Players {
-    ArrayList<Player> listPlayer = new ArrayList();
-
+    ArrayList<Player> players = new ArrayList();
+    private int currentPlayer = 0;
 
     public void add(String playerName) {
-        listPlayer.add(new Player(playerName));
+        players.add(new Player(playerName));
         System.out.println(playerName + " was added");
-        System.out.println("They are player number " + listPlayer.size());
+        System.out.println("They are player number " + players.size());
     }
 
-    public Player getPlayerByIndex(int index) {
-        return listPlayer.get(index);
+    public Player getCurrentPlayer() {
+        return players.get(currentPlayer);
     }
 
-    public int size() {
-        return listPlayer.size();
+    public void nextPlayer() {
+        currentPlayer++;
+        if (currentPlayer == players.size()) {
+            currentPlayer = 0;
+        }
     }
-
 }
